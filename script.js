94fc9e180eac2efb8a40600834db82e55135a03f
T3a4 <tabatha.mfrsouza@senacsp.edu.br>
2025-06-27 08:06:10 -0300

    Ajustar a reorganização da pasta

--- script.js
+++ script.js
@@ -8,9 +8,13 @@ function AtivarPikachu() {
 function AtivarCharmander() {
    document.getElementById("pokemon-name").innerHTML="Charmander";
    document.getElementById("pokemon-img").src="images/charmander.png";
+   document.getElementById("pokemon-type").innerHTML="Fogo";
+   document.getElementById("attacks-list").innerHTML="Lança-chamas", "Garra de Fogo", "Investida";
 }
 
 function AtivarSquirtle() {
    document.getElementById("pokemon-name").innerHTML="Squirtle";
    document.getElementById("pokemon-img").src="images/squirtle.png";
+   document.getElementById("pokemon-type").innerHTML="Água";
+   document.getElementById("attacks-list").innerHTML="Jato d'Água", "Bolhas", "Cauda de Água";
 }
