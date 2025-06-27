878d67905417eaaffa55235c691248d57dff1d47
T3a4 <tabatha.mfrsouza@senacsp.edu.br>
2025-06-27 08:25:08 -0300

    Ajustar a reorganização da pasta

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function AtivarPikachu() {
    document.getElementById("pokemon-name").innerHTML="Pikachu";
    document.getElementById("pokemon-img").src="images/pikachu.png";
    document.getElementById("pokemon-type").innerHTML="Elétrico";
-   document.getElementById("attacks-list").innerHTML="Choque do Trovão", "Ataque Rápido", "Investida Elétrica";
+   document.getElementById("attacks-list").innerHTML="Choque do Trovão<br>Ataque Rápido<br>Investida Elétrica";
 }
 
 function AtivarCharmander() {
    document.getElementById("pokemon-name").innerHTML="Charmander";
    document.getElementById("pokemon-img").src="images/charmander.png";
    document.getElementById("pokemon-type").innerHTML="Fogo";
-   document.getElementById("attacks-list").innerHTML="Lança-chamas", "Garra de Fogo", "Investida";
+   document.getElementById("attacks-list").innerHTML="Lança-chamas<br>Garra de Fogo<br>Investida";
 }
 
 function AtivarSquirtle() {
    document.getElementById("pokemon-name").innerHTML="Squirtle";
    document.getElementById("pokemon-img").src="images/squirtle.png";
    document.getElementById("pokemon-type").innerHTML="Água";
-   document.getElementById("attacks-list").innerHTML="Jato d'Água", "Bolhas", "Cauda de Água";
+   document.getElementById("attacks-list").innerHTML="Jato d'Água<br>Bolhas<br>Cauda de Água";
 }
